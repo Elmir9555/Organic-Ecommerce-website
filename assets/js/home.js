fcf44944f98e63ee1fd31ea1571c271977ede167
Elmir9555 <elmiriu@code.edu.az>
2022-05-11 17:09:55 +0400

    end of header

---
+++ assets/js/home.js
@@ -0,0 +1,41 @@
+//header start ALL CATEGORIES dropdown
+  $(document).ready(function(){
+
+    $(".dropbtns").click(function(){
+      console.log("salam");
+      $("#myDropdown").toggle(1000);
+    });
+  
+  });
+  
+
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtns')) {
+      var dropdowns = document.getElementsByClassName("dropdown-contents");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  //header end ALL CATEGORIES dropdown
+
+
+  //start searchfilter
+
+  $(document).ready(function(){
+
+   $("#all-categ").click(function(){
+     $(".dropdown-content-cate").toggle(800);
+   })
+  
+  });
+
+
+  //end searchfilter
