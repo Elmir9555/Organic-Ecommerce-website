cfbe603ccecc24faa2aea04d77be85c659f37e7b
Elmir9555 <elmiriu@code.edu.az>
2022-05-13 23:10:33 +0400

    end  of tab- menu

--- assets/js/home.js
+++ assets/js/home.js
@@ -46,6 +46,13 @@
     loop:true,
     margin:10,
     nav:true,
+    dots:false,
+
+    smartSpeed:1500,
+    animateIn:'linear',
+    animateOut:'linear',
+
+
     responsive:{
         0:{
             items:1
@@ -59,3 +66,36 @@
     }
  })
  //owl-carousel start
+
+
+
+ //tab-menu -start
+  let catnames =document.querySelectorAll(".category-name div")
+ let content = document.querySelectorAll(".picture")
+
+ console.log(content);
+
+
+  catnames.forEach(catname => {
+    catname.addEventListener("click",function(){   
+      let actived=document.querySelector(".active");
+      actived.classList.remove("active");
+      this.classList.add("active");
+
+      content.forEach(contnt =>{
+        if (this.getAttribute("data-id")!=contnt.getAttribute("data-id")) {
+          contnt.classList.add("d-none")
+        }
+
+        else{
+          contnt.classList.remove("d-none")
+        }
+      })
+   
+      
+
+
+    })
+    
+  });
+ //tab-menu end
