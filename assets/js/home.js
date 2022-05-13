8ee597c6146f6f3f070ab25b9452a8f200fd2141
Elmir9555 <elmiriu@code.edu.az>
2022-05-13 14:17:51 +0400

    end of owl-carousel

--- assets/js/home.js
+++ assets/js/home.js
@@ -35,10 +35,27 @@
      $(".dropdown-content-cate").toggle(800);
     
    })
-
-  
-  
   });
 
 
   //end searchfilter
+
+  //owl-carousel start
+  
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+ })
+ //owl-carousel start
